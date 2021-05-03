3cadce70b2ad10dc73d6075b9762ff5a04709e0e
AntonHarkevich <antonharkevich2002@mail.ru>
2021-05-04 00:26:17 +0300

    first try after message about git

---
+++ mainapp/templatetags/specifications.py
@@ -0,0 +1,50 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+
+TABLE_HEAD ="""
+                <table class="table">
+                    <tbody>
+            """
+
+
+TABLE_TAIL ="""
+                    </tbody>
+                </table>
+            """
+
+
+TABLE_CONTENT = """
+                    <tr>
+                        <td>{name}</td>
+                        <td>{value}</td>
+                    </tr>
+                """
+
+PRODUCT_SPEC = {
+    'pizzaproduct': {
+        "Размер":'size', 
+        'Борт':"board", 
+        "Тесто": 'dough', 
+        "Вегетарианская":'vegetarian'
+    },
+    'beerproduct': {
+        "Цвет":'colour', 
+        'Крепость':"alcohol_strength", 
+        "Фильтрация": 'filtered', 
+        "Сорт":'grade'
+    }     
+}
+
+def get_product_spec(product, model_name):
+    table_content = ''
+    for name, value in PRODUCT_SPEC[model_name].items():
+        table_content += TABLE_CONTENT.format(name=name, value=getattr(product, value))
+    return table_content
+
+@register.filter
+def product_spec(product):
+    model_name = product.__class__._meta.model_name
+    return mark_safe(TABLE_HEAD + get_product_spec(product, model_name) + TABLE_TAIL)
