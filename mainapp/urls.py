bd1a109026ac1a5745994403d3faffbe784e7c39
AntonHarkevich <antonharkevich2002@mail.ru>
2021-05-04 15:47:35 +0300

    cascade of site complete

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,7 +7,8 @@ from .views import (
     AddToCartView, 
     DeleteFromCartView,
     ChangeQTYView,
-    CheckoutView
+    CheckoutView,
+    MakeOrderView
 )
 
 urlpatterns = [
@@ -18,7 +19,8 @@ urlpatterns = [
     path('add-to-cart/<str:ct_model>/<str:slug>/',AddToCartView.as_view(), name='add_to_cart'),
     path('delete-from-cart/<str:ct_model>/<str:slug>/',DeleteFromCartView.as_view(), name='delete_from_cart'),
     path('change-qty/<str:ct_model>/<str:slug>/',ChangeQTYView.as_view(), name='change_qty'),
-    path('checkout/', CheckoutView.as_view(), name='checkout')
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('makeorder/', MakeOrderView.as_view(), name='make_order')
 ]
 
 
